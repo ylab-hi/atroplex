85dedde321292b0e8f475d48e3f0fcdf5edde074
riasc <richard.schaefer@zoho.com>
2025-12-12 13:55:19 -0600

    initial commit - basic project structure

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2025 Richard A. Schäfer
+ *
+ * This file is part of genogrove and is licensed under the terms of the MIT
+ * license. See the LICENSE file in the root of the repository for more
+ * information.
+ */
+
+// standard
+#include <iostream>
+#include <string>
+
+// cxxopts
+#include <cxxopts.hpp>
+
+// class
